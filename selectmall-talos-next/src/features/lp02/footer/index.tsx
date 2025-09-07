5993f9e977d318d94a3f3953f7a04a8832e3263b
Ryot21 <rh.bicmac@gmail.com>
2025-09-08 02:13:03 +0900

    Footer > 修正

--- selectmall-talos-next/src/features/lp02/footer/index.tsx
+++ selectmall-talos-next/src/features/lp02/footer/index.tsx
@@ -3,36 +3,36 @@ import Link from "next/link";
 
 export default function LpFooter() {
   return (
-    <footer id="footer" className={"c-footer -bg-color02 pdt2 pdt5s"}>
+    <footer id="footer" className={"c-footer -lp-color01 pdt2 pdt5s"}>
       <div className={"c-footer-wrapper"}>
         {/* サイト内リンク */}
         <nav className={"c-fnav mgb3 mgb2s"}>
           <ul className={"c-fnav--lists__lp"}>
-            {/* #商品紹介 */}
+            {/* #Talosについて */}
             <li className={"LpnavItem"}>
               <Link
                 href="#sc01"
                 className={`c-fnav--link s-M -b -white -ls-2 -ws-n`}
               >
-                商品紹介
+                Talosについて
               </Link>
             </li>
-            {/* #選ばれる理由 */}
+            {/* #商品紹介 */}
             <li className={"LpnavItem"}>
               <Link
                 href="#sc02"
                 className={`c-fnav--link s-M -b -white -ls-2 -ws-n`}
               >
-                選ばれる理由
+                商品紹介
               </Link>
             </li>
-            {/* #会社概要 */}
+            {/* #ワンウェイケグの特徴 */}
             <li className={"LpnavItem"}>
               <Link
                 href="#sc03"
                 className={`c-fnav--link s-M -b -white -ls-2 -ws-n`}
               >
-                会社概要
+                ワンウェイケグの特徴
               </Link>
             </li>
             <li className={"LpnavItem"}>
@@ -49,7 +49,7 @@ export default function LpFooter() {
         <div className={"logo -lp mgb3 mgb5s"}>
           <Link href="#top" className={"c-link -img"}>
             <Image
-              src="/images/logo/selectmall-logo-w.svg"
+              src="/images/logo/selectmall-logo_old_w.svg"
               alt="セレクトモール"
               width={210}
               height={60}
