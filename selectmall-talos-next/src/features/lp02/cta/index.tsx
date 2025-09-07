647a95ae00a86a5762fff1b4d02e7d261410ef38
Ryot21 <rh.bicmac@gmail.com>
2025-09-08 00:17:36 +0900

    お問い合わせコンテンツ

--- selectmall-talos-next/src/features/lp02/cta/index.tsx
+++ selectmall-talos-next/src/features/lp02/cta/index.tsx
@@ -3,7 +3,7 @@ import Button from "@/components/elements/button";
 
 export default function Lp02Cta() {
   return (
-    <div className="c-contents -Lp02Cta pdt7 pdt15s pdb10 pdb15s">
+    <div className="c-contents -Lp02Cta pdt7 pdt15s pdb7 pdb15s">
       <div className="c-contents--inner">
         <ul className="c-flex -col1_2 -pc1_2 -h-top -jc-sb">
           <li className="flexItem mgb10s" style={{ zIndex: 3 }}>
@@ -11,23 +11,25 @@ export default function Lp02Cta() {
               もっと詳しく知りたい方へ
             </h2>
             <p className="s-M -white -left -ls-2 -lh-2 mgb5 mgb10s">
-              マネリペの特徴や価格、事例に加えて、社員定着を成功に導いた豊富なノウハウをもとに、貴社に合わせたプランを実際の画面をご覧いただきながら、わかりやすくご説明・ご提案いたします。
+            「ステンレスケグと迷っている」「他社のワンウェイケグと比較したい」という方も遠慮なくご相談をお待ちしております。お客様に本当に最適な選択肢をメリット・デメリットも含めて正直にお伝えします。
             </p>
             <Button
-              href="lp02#contact"
+              href="/#contact"
               customAreaClass="-left"
               customClass="-btn -maru -orange -icon -arrow"
             >
-              資料ダウンロードはこちら
+              お問い合わせはコチラ
             </Button>
           </li>
           <li className="flexItem" style={{ zIndex: 3 }}>
-            <Image
-              src="/images/Lp/panf-2024.png"
-              alt="マネリペの冊子 2024年版"
-              width={480}
-              height={320}
-            />
+            <div className="c-img">
+              <Image
+                src="/images/keg/tkeg&brewjet.png"
+                alt="Talosケグのラインナップ豊富"
+                width={480}
+                height={320}
+              />
+            </div>
           </li>
         </ul>
       </div>
