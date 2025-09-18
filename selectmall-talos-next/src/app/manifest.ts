aa34413d47a9be568733ca6f00adeeb7e5b1d3ce
Ryot21 <rh.bicmac@gmail.com>
2025-09-19 03:25:20 +0900

    Update manifest.ts

--- selectmall-talos-next/src/app/manifest.ts
+++ selectmall-talos-next/src/app/manifest.ts
@@ -20,7 +20,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/images/sns/apple-touch-icon.png",
         sizes: "180x180",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/favicon.ico",
